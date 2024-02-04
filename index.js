820ee286d4745614f6f9639ecc0a3640c95f8087
up1 <somkiat.p@gmail.com>
2024-02-04 09:11:10 +0700

    Allow cross-origin

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+// Middleware
+app.use(express.json())
+
+// Allowing CORS
+app.use(cors("*"))
 
 app.get('/', (req, res) => {
   res.json({ 
