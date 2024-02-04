435ac98d999e03b5877a6f1d3d6f0961e2cf545e
up1 <somkiat.p@gmail.com>
2024-02-04 07:56:02 +0700

    change response to json format

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from vercel!')
+  res.json({ message: 'Hello World!' })
 })
 
 app.listen(port, () => {
